9ded7f38dd6c017bba84f907f6f5b9cb5f258232
springkim <springnode@gmail.com>
2020-02-21 17:01:53 +0900

    working...

---
+++ prj_cip/package_list.cpp
@@ -0,0 +1,9 @@
+/*
+*  package_list.cpp
+*  cip
+*
+*  Created by kimbomm on 2020. 2. 21...
+*  Copyright 2020 kimbomm. All rights reserved.
+*
+*/
+#include"package_list.h"
---
+++ prj_cip/package_list.h
@@ -0,0 +1,27 @@
+/*
+*  package_list.h
+*  cip
+*
+*  Created by kimbomm on 2020. 2. 21...
+*  Copyright 2020 kimbomm. All rights reserved.
+*
+*/
+#if !defined(CIP_7E4_2_15_PACKAGE_LIST_H_INCLUDED)
+#define CIP_7E4_2_15_PACKAGE_LIST_H_INCLUDED
+#include<iostream>
+#include<vector>
+#include<string>
+struct Package{
+	std::string version;
+	std::vector<std::string> include_files;
+	std::vector<std::string> lib_files;
+	std::vector<std::string> dll_files;
+};
+class PackageList{
+	std::vector<Package> packages;
+	void Read(std::string path){
+		
+	}
+};
+
+#endif  //CIP_7E4_2_15_PACKAGE_LIST_H_INCLUDED
